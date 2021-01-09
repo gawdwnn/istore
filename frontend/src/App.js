eeb00f73c46fbac4f5a3b5f9a360efd1f9127f6b
smarttin <smarttin77@gmail.com>
2021-01-09 14:02:39 +0100

    using react-router and productDetails screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
 import {Container} from 'react-bootstrap';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
-          <HomeScreen/>
+          <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
