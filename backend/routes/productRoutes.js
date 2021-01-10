1a779aea343b743d0288d77d2b568477d6cee04f
smarttin <smarttin77@gmail.com>
2021-01-10 22:19:18 +0100

    custom error handler

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import Product from '../models/productModel.js';
+
+const router = express.Router();
+
+router.get(
+  '/',
+  expressAsyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  }),
+);
+
+router.get(
+  '/:id',
+  expressAsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error('Product not found');
+    }
+  }),
+);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
-import products from './data/products.js';
 import connectDB from './config/database.js';
+import productRoutes from './routes/productRoutes.js';
+import {errorHandler, notFound} from './middleware/errorMiddleware.js';
 
 dotenv.config();
 connectDB();
@@ -10,14 +11,9 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(
   PORT,
