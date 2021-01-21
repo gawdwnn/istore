4d8415c57baed0ccb1e0313f706e96ec6bd90137
smarttin <smarttin77@gmail.com>
2021-01-21 15:30:32 +0100

    add paypal payment

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -95,7 +95,7 @@ export const payOrder = (orderId, paymentResult) => async (dispatch, getState) =
       },
     };
 
-    const {data} = await axios.put(`/api/orders/${orderId}/pay`, paymentResult, config);
+    const {data} = await axios.patch(`/api/orders/${orderId}/pay`, paymentResult, config);
 
     dispatch({
       type: ORDER_PAY_SUCCESS,
