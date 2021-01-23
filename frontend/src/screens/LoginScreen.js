54dfbffe996363a9e29c0e5f8099d5eb3d0c2444
smarttin <smarttin77@gmail.com>
2021-01-23 09:34:50 +0100

    prepare for deployment

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -3,7 +3,6 @@ import {Link} from 'react-router-dom';
 import {Form, Button, Row, Col} from 'react-bootstrap';
 import {useDispatch, useSelector} from 'react-redux';
 import Message from '../components/Message';
-import Loader from '../components/Loader';
 import FormContainer from '../components/FormContainer';
 import {login} from '../actions/userActions';
 
